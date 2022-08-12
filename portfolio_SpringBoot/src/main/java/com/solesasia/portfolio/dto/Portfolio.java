7e3618c926957d777ae21b22d1d5d603fd499aad
Soledad Sasia <soledadsasia@gmail.com>
2022-08-12 10:29:46 -0300

    Crea paquete  DTO y dentro clase Portfolio

---
+++ portfolio_SpringBoot/src/main/java/com/solesasia/portfolio/dto/Portfolio.java
@@ -0,0 +1,19 @@
+package com.solesasia.portfolio.dto;
+
+import com.solesasia.portfolio.model.Educacion;
+import com.solesasia.portfolio.model.Persona;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class Portfolio {
+    
+    private Persona persona;
+    private List<Educacion> listaEducacion;
+    private List<Experiencia> listaExperiencia;
+    private List<HabilidadTecnica> listaHabilidadTecnica;
+    private List<HabilidadBlanda> listaHabilidadBlanda;
+    private List<Proyectoa> listaProyecto;
+    
+}
